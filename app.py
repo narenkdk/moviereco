f4cb2827b308550662373bdcf5384d92553358be
Naren Khadka <narenkdk@gmail.com>
2025-01-05 21:55:32 +0545

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def recommend(movie):
     return recommended_movies, recommended_movies_posters
 
 
+# Load data
 movies_dict = pickle.load(open('movies_dict.pkl','rb'))
 movies = pd.DataFrame(movies_dict)
 
@@ -32,52 +33,8 @@ similarity = pickle.load(open('similarity.pkl','rb'))
 st.title('Movie Recommendation System')
 
 selected_movie_name = st.selectbox(
-"Chose your movie",
-movies['title'].values)
-
-if st.button('Show Recommendation'):
-    recommended_movie_names, recommended_movie_posters = recommend(selected_movie_name)
-    cols = st.columns(5)  # Create 5 columns dynamically
-    for i in range(min(5, len(recommended_movie_names))):  # Ensure we don't exceed available recommendations
-        with cols[i]:
-            st.text(recommended_movie_names[i])
-            
-import streamlit as st
-import pickle
-import pandas as pd
-import requests
-
-def fetch_poster(movie_id):
-    response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id))
-    data = response.json()
-    return "https://image.tmdb.org/t/p/w500" + data['poster_path']
-
-def recommend(movie):
-    movie_index = movies[movies['title'] == movie].index[0]
-    distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-
-    recommended_movies = []
-    recommended_movies_posters = []
-    for i in movies_list:
-        movie_id = movies.iloc[i[0]].movie_id
-
-        recommended_movies.append(movies.iloc[i[0]].title)
-        # fetch poster from API
-        recommended_movies_posters.append(fetch_poster(movie_id))
-    return recommended_movies, recommended_movies_posters
-
-
-movies_dict = pickle.load(open('movies_dict.pkl','rb'))
-movies = pd.DataFrame(movies_dict)
-
-similarity = pickle.load(open('similarity.pkl','rb'))
-
-st.title('Movie Recommendation System')
-
-selected_movie_name = st.selectbox(
-"Chose your movie",
-movies['title'].values)
+    "Choose your movie",
+    movies['title'].values)
 
 if st.button('Show Recommendation'):
     recommended_movie_names, recommended_movie_posters = recommend(selected_movie_name)
@@ -86,3 +43,10 @@ if st.button('Show Recommendation'):
         with cols[i]:
             st.text(recommended_movie_names[i])
             st.image(recommended_movie_posters[i])
+
+
+selected_movie_name = st.selectbox(
+    "Choose your movie",
+    movies['title'].values,
+    key='movie_selectbox')  # Unique key for selectbox
+
